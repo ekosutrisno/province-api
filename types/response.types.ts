ea92aa3c7961d817fa2f23d7690099fed5477c77
Eko Sutrisno <eko@briix.com>
2023-08-06 14:08:18 +0700

    feat(api): new endpoint province get all and get by code

---
+++ types/response.types.ts
@@ -0,0 +1,19 @@
+export interface GeneralResponse<T> {
+    version: string;
+    timestamp: number;
+    data: T;
+    metadata: MetaData
+}
+
+
+export interface MetaData {
+    totalData: number;
+    lastSyncYear: number;
+    currentBase: BaseDataReference;
+    baseReferenceAvailable: BaseDataReference[];
+    info: string;
+    level: LevelData;
+}
+
+export type BaseDataReference = 'BPS' | 'KEMENDAGRI';
+export type LevelData = 'PROVINCE' | 'DISTRICT' | 'SUB_DISTRICT' | 'VILLAGE';
