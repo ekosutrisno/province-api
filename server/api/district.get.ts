d338c2634e3b1b17a17493a55127a3ebc4e13cc5
Eko Sutrisno <eko@briix.com>
2023-08-06 14:09:00 +0700

    feat(api): new endpoint get district by province code

---
+++ server/api/district.get.ts
@@ -0,0 +1,27 @@
+
+import { JSONPath } from 'jsonpath-plus'
+import { data, metadata } from '../data/district.json';
+import { BaseDataReference, GeneralResponse, LevelData } from '../../types/response.types';
+
+export default defineEventHandler((event) => {
+  const query = getQuery(event);
+  const base = query.base as BaseDataReference ?? 'BPS';
+  const propertyPathRef = base == 'BPS' ? 'province_code_bps' : 'province_code_dagri';
+  const path = query.province_code ? `$[?(@.${propertyPathRef} === "${query.province_code}" )]` : `$.*`;
+
+  const response: GeneralResponse<typeof data> = {
+    version: 'v0.0.1-BETA',
+    timestamp: Date.now(),
+    data: JSONPath({ path, json: data }),
+    metadata: {
+      currentBase: base,
+      baseReferenceAvailable: metadata.baseReferenceAvailable as BaseDataReference[],
+      lastSyncYear: metadata.latestUpdate,
+      totalData: metadata.total,
+      info: 'Direkomendasikan menggunakan base data BPS',
+      level: metadata.level as LevelData
+    }
+  }
+
+  return response;
+})
